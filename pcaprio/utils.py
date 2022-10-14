3d6d306e3526a3b1abf6ef48c08bb1bba57b5e93
Illia <ilyayrch@gmail.com>
2022-10-14 08:04:26 +0200

    repr_IPv4/6

--- pcaprio/utils.py
+++ pcaprio/utils.py
@@ -9,3 +9,16 @@ def beautiful_hex(hex_data: str) -> str:
         res.append(' '.join([row[i:i+2] for i in range(0, len(row), 2)]))
     
     return '\n'.join(res) + " "
+
+
+def repr_IPv4(raw: bytes) -> str:
+    return '.'.join([f'{b}' for b in raw])
+
+
+def repr_IPv6(raw: bytes) -> str:
+    res = []
+
+    for i in range(0, len(raw), 2):
+        part = f'{raw[i]:02x}{raw[i+1]:02x}'
+        res.append(part)
+    return ':'.join(res)
